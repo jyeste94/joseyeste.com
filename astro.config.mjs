130906dd8e3e00bee0d71364ec7a85b564ba33cc
Jose Yeste <yeste@gac.travel>
2026-09-04 14:32:53 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://joseyeste.com',
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt()
+  ]
 })
